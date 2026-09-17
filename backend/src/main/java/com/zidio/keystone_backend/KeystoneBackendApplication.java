7618cabdb536e073f5b9fe8a8f7155b1209de2be
theboogyman15 <thevishalrathod2005@gmail.com>
2026-09-18 02:35:00 +0530

    Rename backend folder, fix gitignore

---
+++ backend/src/main/java/com/zidio/keystone_backend/KeystoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zidio.keystone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeystoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeystoneBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zidio/keystone_backend/KeystoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zidio.keystone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeystoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
